902c4ed4d2d11e79e228cd06427769a49658b1ce
xuewei619 <532640777@qq.com>
2016-05-29 21:49:32 +0800

    update

--- ll1.py
+++
@@ -1,6 +0,0 @@
-#-*- encoding:utf-8 -*-
-'''
-Created on 2016年5月27日
-
-@author: xuewei
-'''
--- test.py
+++
@@ -1,9 +0,0 @@
-#-*- encoding:utf-8 -*-
-'''
-Created on 2016年5月27日
-
-@author: xuewei
-'''
-import ll1
-
-ll1.haha()
